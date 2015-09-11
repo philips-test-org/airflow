b34251d78ba87b99b3bc31a8a02d5f4ff680df55
= <treigel@analytical.info>
2015-09-11 11:20:21 -0400

    add demo controller

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,24 @@
+class DemoController < ApplicationController
+  before_filter :general_authentication
+  before_filter :get_entity_manager
+  after_filter :close_entity_manager
+
+  def index
+    @exams = []
+
+    if params[:accession]
+      @exams = search_by_accession(params[:accession])
+      @acc_search = params[:accession]
+    end
+  end
+
+  def search
+  end
+
+  private
+  def search_by_accession(accession)
+    query = Java::HarbingerSdkData::RadExam.createQuery(@em)
+    query.where(query.equal(".accession", query.literal(accession))).list.to_a
+  end
+
+end
