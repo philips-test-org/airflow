7f386698e2123f7fe66a91ff4e772f0f22dc5954
= <treigel@analytical.info>
2015-09-11 11:19:43 -0400

    remove content from main controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,9 +4,6 @@ class MainController < ApplicationController
   after_filter :close_entity_manager
 
   def index
-    query = Java::HarbingerSdkData::RadExam.createQuery(@entity_manager)
-    query.where([query.equal(".siteClass.ed",true),query.greaterThan(".radExamTime.endExam",1.day.ago.to_time)])
-    @exams = query.order(".radExamTime.endExam desc").list()
   end
 
   def help
