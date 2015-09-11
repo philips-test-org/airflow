45d9abed11d046c1e32b81744e632ac10a0a584f
= <treigel@analytical.info>
2015-09-11 09:34:44 -0400

    remove wildcard match

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 VanillaApplication::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-  match 'about' => 'main#about'
+  get 'about' => 'main#about'
 
   # Vanilla of regular route:
   #   match 'products/:id' => 'catalog#view'
@@ -52,8 +52,4 @@ VanillaApplication::Application.routes.draw do
   root :to => 'main#index'
 
   # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  match ':controller(/:action(/:id))(.:format)'
 end
