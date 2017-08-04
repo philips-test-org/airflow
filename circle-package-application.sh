a8ccdb06ef1509a142973c05465ec419b42c043d
Christopher Toland <ctoland@analytical.info>
2017-08-04 17:18:34 -0400

    added circle package script

---
+++ circle-package-application.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+CWD=`pwd`
+APPNAME=`cat config/application.name`
+VERSION=`git describe --always`
+WARFILE="$APPNAME.war.$VERSION"
+PACKAGEDIR="$APPNAME-$VERSION"
+
+echo "Warblizer: Creating version page"
+git describe --always > app/views/main/_version.html.erb
+
+echo "Warblizer: Bundling"
+WARRING="warbilizing" RAILS_ENV=production jruby -S bundle install --path=target
+
+echo "Warblizer: Pre Cleaning Assets"
+WARRING="warbilizing" ASSETS="clean" RAILS_ENV=production jruby -J-XX:MaxPermSize=256m -J-Xmx4096m -S bundle exec rake assets:clean
+
+echo "Warblizer: Precompiling Assets (this takes a while)"
+ASSETS="precompile" RAILS_RELATIVE_URL_ROOT="/$APPNAME" RAILS_ENV=production jruby -J-XX:MaxPermSize=256m -J-Xmx4096m -S bundle exec rake assets:precompile
+
+echo "Warblizer: Building War File"
+WARRING="warbilizing" RAILS_ENV=production jruby -S bundle exec warble war
+
+echo "Warblizer: Changing permissions and war file name"
+chmod +r "$APPNAME.war"
+mv -v "$APPNAME.war" "$APPNAME.war.`git describe --always`"
+
+mkdir -v $PACKAGEDIR
+cp -v CHANGELOG.md $PACKAGEDIR
+cp -v INSTALL.md $PACKAGEDIR
+cp -v $WARFILE $PACKAGEDIR
+zip -rq $PACKAGEDIR.zip $PACKAGEDIR
+rm -rf $PACKAGEDIR
+echo "Packager: Created $PACKAGEDIR.zip"
