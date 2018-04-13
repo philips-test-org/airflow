20c179d61a23bcc7e4d7ddcb4dae6867dfea6546
Travis Poulsen <travis.poulsen@philips.com>
2018-04-13 10:58:25 -0400

    Remove references to session reducer
    Not in use currently.

--- web/app/lib/reducers/index.js
+++ web/app/lib/reducers/index.js
@@ -1,12 +1,10 @@
 // @flow
 import {combineReducers} from "redux";
 import board from "./board";
-import session from "./session";
 import user from "./user";
 
 const rootReducer = combineReducers({
   board,
-  session,
   user,
 });
 
