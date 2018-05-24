bf567425b155cebd05e6d5bc0aeb9638f0251bcf
Travis Poulsen <travis.poulsen@gmail.com>
2018-05-24 10:28:18 -0400

    Revert "Redirect to login (refresh) if trying to fetch when logged out"
    Reverting due to errors on POST requests; also rethinking in the event that the
    app and openam are on the same server.
    
    This reverts commit 2078bb933dba3c67a2e2327be76db6c303c36d47.

--- web/app/lib/network.js
+++ web/app/lib/network.js
@@ -1,6 +1,5 @@
 // @flow
 /* eslint no-console: 0 */
-/* global location */
 import * as R from "ramda";
 
 const HEADERS = new Headers({
@@ -13,12 +12,10 @@ export const GET = (url: string, params: Object = {}) => {
   const queryString = buildQueryString(params, newParams);
   const urlWithParams = `${url}?${queryString}`;
   return fetch(urlWithParams, {
-    mode: "no-cors",
     method: "GET",
     headers: HEADERS,
     credentials: "include",
   }).then((response) => {
-    refreshIfOpaque(response);
     if (response.ok) {
       if (R.contains("application/json", response.headers.get("content-type"))) {
         return response.json();
@@ -39,14 +36,12 @@ export const POST = (url: string, params: Object = {}) => {
   const csrfToken = getCSRFToken();
   HEADERS.set("X-CSRF-TOKEN", csrfToken);
   return fetch(url, {
-    mode: "no-cors",
     method: "POST",
     headers: HEADERS,
     credentials: "same-origin",
     //credentials: "include",
     body: JSON.stringify(params),
   }).then((response) => {
-    refreshIfOpaque(response);
     if (response.ok) {
       if (R.contains("application/json", response.headers.get("content-type"))) {
         return response.json();
@@ -85,12 +80,4 @@ const getCSRFToken = () => {
   // Suppressing the error for now.
   // $FlowFixMe
   return selector.content;
-}
-
-// Redirect the user to login if the response is opaque
-// (they were logged out and a login page was returned)
-const refreshIfOpaque = (response) => {
-  if (response.type === "opaque") {
-    location.reload();
-  }
 }
