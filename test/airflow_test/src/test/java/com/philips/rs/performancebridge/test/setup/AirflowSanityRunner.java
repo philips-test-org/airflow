b0fa14f907c81464b9fd98a7f3557f6e4cc12e87
Kaur <jasmine.kaur@philips.com>
2018-07-12 14:31:47 +0530

    #48707 - Airflow Sanity folder structure

---
+++ test/airflow_test/src/test/java/com/philips/rs/performancebridge/test/setup/AirflowSanityRunner.java
@@ -0,0 +1,22 @@
+package com.philips.rs.performancebridge.test.setup;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		format   = {"pretty","html:target/cucumber", "json:target/cucumber.json","rerun:target/rerun_featureName.txt"},
+		features = {"src/test/resources/com.philips.airflow.sanity.feature"},								
+		strict = true,
+	    monochrome = true,
+	    
+        glue = {"com.philips.airflow.sanity.stepdefs","com.philips.airflow.sanity.utils"},
+        		tags = {"@airflow"}
+        
+		)
+
+public class AirflowSanityRunner {
+
+}
