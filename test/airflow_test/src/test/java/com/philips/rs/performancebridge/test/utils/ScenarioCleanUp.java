9544e12a79af72d0f07c7d0ad77e4bec605b55a4
Kaur <jasmine.kaur@philips.com>
2018-07-19 11:12:37 +0530

    #48707: Airflow Sanity Scenario Cleanup

--- test/airflow_test/src/test/java/com/philips/rs/performancebridge/test/utils/ScenarioCleanUp.java
+++ test/airflow_test/src/test/java/com/philips/rs/performancebridge/test/utils/ScenarioCleanUp.java
@@ -3,6 +3,8 @@ package com.philips.rs.performancebridge.test.utils;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import org.junit.Assert;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 
@@ -12,16 +14,17 @@ import cucumber.api.java.Before;
 
 public class ScenarioCleanUp {
 
+	public static final Logger logger = Logger.getLogger(ScenarioCleanUp.class);
+	public static WebDriver driver = UITestUtils.driver;
 	public static String scname = "";
 	public static String featurename = "";
-	public static final Logger logger = Logger.getLogger(ScenarioCleanUp.class);
-	public WebDriver driver = UITestUtils.driver;
 
 	@After()
 	public void tearDown(Scenario scenario) throws Exception {
 		System.out.println("---------Closing the Sceanrio-----------------");
-		// boolean scenarioCleanUp = false;
+		boolean scenarioCleanUp = false;
 
+		driver = UITestUtils.driver;
 		if (scenario.isFailed()) {
 			System.out.println("Came Inside Scenario Failed");
 			DateFormat df = new SimpleDateFormat("ddMMyyHHmmss");
@@ -29,9 +32,8 @@ public class ScenarioCleanUp {
 			logger.info("CurrentDate used for concating the sceenshot: " + df.format(dateobj));
 
 			logger.error("The Scenario is failed: " + scenario.getName());
-			if (UITestUtils.driver != null) {
+			if (driver != null) {
 				scenario.write("Current Page URL is " + driver.getCurrentUrl());
-
 				String filename = scenario.getName();
 				filename = filename.replaceAll("\"", "");
 				filename = filename.replaceAll("\\s+", "_");
@@ -44,9 +46,14 @@ public class ScenarioCleanUp {
 	}
 
 	@Before()
+	public void startUp(Scenario scenario) {
+		logger.info("Scenario Name :" + scenario.getName());
+	}
+
+	@Before()
 	public static String getTheScenarioName(Scenario scenario) {
-		logger.info("sceanrio Names :"+scenario.getName());
 		return scname = scenario.getName();
+
 	}
 
 	@Before()
@@ -54,7 +61,9 @@ public class ScenarioCleanUp {
 
 		scenario.getSourceTagNames();
 		String rawFeatureName = scenario.getId().split(";")[0].replace("-", " ");
+
 		featurename = rawFeatureName.substring(0, 1).toUpperCase() + rawFeatureName.substring(1);
+
 		return featurename;
 	}
 }
