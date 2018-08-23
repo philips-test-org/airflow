164903608a7a6ea3b8befe89c95d22d99bbe6637
Suman, Swarnim <swarnim.suman@philips.com>
2018-08-23 11:24:57 +0530

    #46575 Making Font-Awesome changes

--- warblizer.sh
+++ warblizer.sh
@@ -9,16 +9,16 @@ git describe --always > app/views/main/_version.html.erb
 # Warbalizing with changed files
 
 echo "Warblizer: Bundling"
-RAILS_ENV=production jruby -J-XX:MaxPermSize=256m -S bundle install --path=target
+RAILS_ENV=production jruby -S bundle install --path=target
 
 echo "Warblizer: Pre Cleaning Assets"
 ASSETS="clean" RAILS_ENV=production jruby -S bundle exec rake assets:clean
 
 echo "Warblizer: Precompiling Assets (this takes a while)"
-ASSETS="precompile" RAILS_RELATIVE_URL_ROOT="/$APPNAME" RAILS_ENV=production jruby -J-XX:MaxPermSize=256m -S bundle exec rake assets:precompile
+JRUBY_OPTS="-J-Xmx1024m" ASSETS="precompile" RAILS_RELATIVE_URL_ROOT="/$APPNAME" RAILS_ENV=production -S bundle exec rake assets:precompile
 
 echo "Warblizer: Building War File"
-WARRING="warbilizing" RAILS_ENV=production jruby -S bundle exec warble war
+WARRING="warbilizing" RAILS_ENV=production jruby -S bundle exec warble compiled war
 
 echo "Warblizer: Post Cleaning Assets"
 ASSETS="clean" RAILS_ENV=production jruby -S bundle exec rake assets:clean
